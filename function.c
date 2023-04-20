9500151acf71daccdb8c2afc7cdaafc142043e7c
DarKreter <darkreter@gmail.com>
2023-04-20 09:17:24 +0900

    reformat

---
+++ function.c
@@ -0,0 +1,32 @@
+#include "function.h"
+
+char* Validate(int* argc, char** argv)
+{
+    FILE* fptr;
+    if(*argc < 2) {
+        printf("No command-line arguments!\n");
+        exit(1);
+    }
+    else if(*argc > 2) {
+        printf("Too many command-line arguments!\n");
+        exit(2);
+    }
+
+    // printf("Reading file %s\n", argv[1]);
+    if((fptr = fopen(argv[1], "r")) == NULL) {
+        printf("Cannot open file!");
+        exit(3);
+    }
+    fseek(fptr, 0L, SEEK_END);
+    int sz = ftell(fptr);
+    printf("%d\n", sz);
+    fseek(fptr, 0L, SEEK_SET);
+
+    char* tab = malloc(1000 * sizeof(char));
+    fscanf(fptr, "%s", tab);
+
+    fclose(fptr);
+
+    printf("%s", tab);
+    return tab;
+}
---
+++ function.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_BRAINFUCK
+#define FUNCTION_BRAINFUCK
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char* Validate(int* argc, char** argv);
+
+#endif
