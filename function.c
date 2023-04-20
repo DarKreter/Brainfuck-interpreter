145a51565101d0a8f81c36b5a96fc897926b1e6a
DarKreter <darkreter@gmail.com>
2023-04-20 12:19:53 +0900

    Comment code

--- function.c
+++ function.c
@@ -13,15 +13,19 @@ char* Validate(int* argc, char** argv)
     }
 
     // printf("Reading file %s\n", argv[1]);
+    // Open file
     if((fptr = fopen(argv[1], "r")) == NULL) {
         fputs("Cannot open file!\n", stderr);
         exit(3);
     }
+    // Check length
     fseek(fptr, 0L, SEEK_END);
     int file_size = ftell(fptr);
     // printf("%d\n", file_size);
+    // Move back to beginning
     fseek(fptr, 0L, SEEK_SET);
 
+    // Alocate memory for program code
     char* tab = malloc(file_size * sizeof(char));
     if(!tab) {
         fclose(fptr);
@@ -29,6 +33,7 @@ char* Validate(int* argc, char** argv)
         exit(4);
     }
 
+    // Read it from file
     if(fread(tab, file_size, 1, fptr) != 1) {
         fclose(fptr);
         free(tab);
@@ -36,6 +41,7 @@ char* Validate(int* argc, char** argv)
         exit(4);
     }
 
+    // Set last byte to zero
     tab[file_size] = 0;
     fclose(fptr);
 
@@ -46,11 +52,15 @@ char* Validate(int* argc, char** argv)
 void SkipForward(char** program)
 {
     int i = 0;
+    // In case we go too far
     while(**program != 0) {
         (*program)++;
         // printf("-%c\n", **program);
+        // If there is another opening bracket increment i
         if((**program) == '[')
             i++;
+        // if there is closing bracket decrement i or end loop if its
+        // our corresponding bracket
         else if((**program) == ']') {
             if(i > 0)
                 i--;
@@ -64,11 +74,14 @@ void SkipForward(char** program)
 void SkipBack(char** program, char* limit)
 {
     int i = 0;
+    // In case we go before start of the heap
     while(*program != limit) {
         (*program)--;
         // printf("-%c\n", **program);
+        // Count ending brackets
         if((**program) == ']')
             i++;
+        // Count opening bracket and check for corresponding bracket
         else if((**program) == '[') {
             if(i > 0)
                 i--;
--- main.c
+++ main.c
@@ -4,34 +4,48 @@
 
 int main(int argc, char* argv[])
 {
+    // Check command-line arguments, read brainfuck program
     char *program = Validate(&argc, argv), *limit = program;
+    // Allocate memory
     char *working_space = calloc(WS_SIZE, sizeof(char)),
          *ptr = working_space;
+    char move_on = 1;
 
+    // Execute each letter
     while(*program != 0) {
         // printf("%c ", *(program));
         switch(*program) {
         case '+':
+            // Add 1 to memory cell
             (*ptr)++;
             break;
         case '-':
+            // Substract 1 from memory cell
             (*ptr)--;
             break;
         case '.':
+            // print pointed memory cell
             printf("%c", *ptr);
             break;
         case ',':
+            // read to pointer memory cell
             scanf("%c", ptr);
             break;
         case '>':
+            // Move to next cell
+            // But only if we are not exceeding memory heap
             if(ptr != working_space + WS_SIZE - 1)
                 ptr++;
             break;
         case '<':
+            // move back
+            // But not before start of allocated heap
             if(ptr != working_space)
                 ptr--;
             break;
         case '[':
+            // Check if pointed memory cell is 0
+            // If it is 0 then skip to closing bracket
             if(!(*ptr)) {
                 // printf("Skip forward!\n");
                 // printf("ss %c\n", *program);
@@ -41,10 +55,13 @@ int main(int argc, char* argv[])
             break;
         case ']':
             // Skip back to opening bracket
+
             // printf("Skip back!\n");
             // printf("ss %c\n", *program);
             SkipBack(&program, limit);
-            program--;
+            // Do not increment over opening bracket to check
+            // condition of the loop
+            move_on = 0;
             // printf("se %c\n", *program);
             // scanf("LOL:%c");
             break;
@@ -53,9 +70,11 @@ int main(int argc, char* argv[])
         case '\t':
             break;
         default:
-            printf("'%c'", *program);
+            printf("Unrecognized character: %c\n", *program);
         }
-        program++;
+        if(move_on)
+            program++;
+        move_on = 1;
     }
 
     return 0;
